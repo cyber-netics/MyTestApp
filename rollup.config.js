df87ec12e354610f91a355bd88b800a163eefafa
Eduard  Jacobs <eduardjacobs@Eduards-MacBook-Pro.local>
2021-11-26 21:37:39 -0800

    test

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
 import { terser } from "rollup-plugin-terser";
 import external from "rollup-plugin-peer-deps-external";
+import typescript from "rollup-plugin-typescript2";
 
 const packageJson = require("./package.json");
 
