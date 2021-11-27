2bce2c33abf722825ecb863da2b8e113d1521b30
Eduard  Jacobs <eduardjacobs@Eduards-MacBook-Pro.local>
2021-11-26 20:45:48 -0800

    v1

--- src/Components/Button/index.tsx
+++ src/Components/Button/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Container } from "./styles";
 
 export interface ButtonProps {
   label: string;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return <Container>{props.label}</Container>;
 };
 
 export default Button;
---
+++ src/Components/Button/styles/style.tsx
@@ -0,0 +1,8 @@
+import { css } from "styled-components";
+
+export const base = css`
+  border-width: 0;
+  border-radius: 2px;
+  position: relative !important;
+  transition: all 0.3s;
+`;
