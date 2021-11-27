b1023e6e9b07be92bdbc05b267ab5da3efe9b602
Eduard  Jacobs <eduardjacobs@Eduards-MacBook-Pro.local>
2021-11-26 21:18:07 -0800

    test

--- build/types/Components/Button/styles/style.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const base: import("styled-components").FlattenSimpleInterpolation;
--- src/Components/Button/index.tsx
+++ src/Components/Button/index.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  position: relative;
+`;
 
 export interface ButtonProps {
   label: string;
 }
 
 const Button = (props: ButtonProps) => {
-  return <div>{props.label}</div>;
+  return <Container>{props.label}</Container>;
 };
 
 export default Button;
