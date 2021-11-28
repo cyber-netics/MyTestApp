5853d2177fb1c710237d50c1ddfa8d6f7252db9c
Eduard Jacobs <edoter92@gmail.com>
2021-11-28 02:44:36 -0800

    test

--- cypress/component/Test.spec.tsx
+++ cypress/component/Test.spec.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import { mount } from "@cypress/react";
 
 const Button: React.FC = () => {
-  return <h1>Hello</h1>;
+  return (
+    <div className="button-elem">
+      <h1>Hello</h1>
+    </div>
+  );
 };
 
 describe("Avatar Sizing", () => {
   it("tesing", () => {
     mount(<Button />);
+    cy.get(".button-elem").should("have.text", "Hello");
   });
 });
