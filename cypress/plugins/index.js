d145c10af344ef36516919de71b5b6b1d8d383f9
Eduard Jacobs <edoter92@gmail.com>
2021-11-28 14:10:06 -0800

    test

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,7 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { startDevServer } = require("@cypress/webpack-dev-server");
-const webpack = require("@cypress/webpack-preprocessor");
-
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const webpackConfig = require("../../webpack.cypress");
 
@@ -9,8 +7,6 @@ module.exports = (on, config) => {
   on("dev-server:start", (options) =>
     startDevServer({ options, webpackConfig })
   );
-
-  // on("file:preprocessor", (options) => webpack(options, webpackConfig));
 
   return config;
 };
