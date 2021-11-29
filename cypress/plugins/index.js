86015c93bd837910b4f986a13141bba451c2fe47
Eduard Jacobs <edoter92@gmail.com>
2021-11-28 17:52:12 -0800

    test

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -2,11 +2,16 @@
 const { startDevServer } = require("@cypress/webpack-dev-server");
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const webpackConfig = require("../../webpack.cypress");
+import { findBrowser } from "./browsers";
 
 module.exports = (on, config) => {
   on("dev-server:start", (options) =>
     startDevServer({ options, webpackConfig })
   );
 
-  return config;
+  return findBrowser().then((browser) => {
+    return {
+      browsers: config.browsers.concat(browser),
+    };
+  });
 };
