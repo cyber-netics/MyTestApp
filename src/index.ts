fd41e933bfd90b87f73522e03af8e8b403e6619a
Eduard Jacobs <edoter92@gmail.com>
2021-11-30 11:36:13 -0800

    test

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export * from "./Components/Button";
+export { default as Badge } from "./Components/Badge";
+// export { default as Checkbox } from "./Components/Checkbox";
+// export { default as Radio } from "./Components/Radio";
+// export { default as Select } from "./Components/Select";
+// export { default as Thumbnail } from "Components/Thumbnail";
+// export * from "Components/Grid";
+// export * from "./Components/Input";
