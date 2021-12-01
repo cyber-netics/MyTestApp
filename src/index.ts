0ed3a80ccf13cc85b16744f059571d2bf1e1fe56
Eduard Jacobs <edoter92@gmail.com>
2021-11-30 16:36:11 -0800

    export components

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-export { default as Button } from "./Components/Button";
+export { default as Button, ButtonProps } from "./Components/Button";
+export { Input, InputEmail, InputNumber, InputProps } from "./Components/Input";
+export { default as Breadcrumb, BreadcrumbProps } from "Components/Breadcrumb";
+
 export { default as Badge } from "./Components/Badge";
 export { default as Checkbox } from "./Components/Checkbox";
 export { default as Radio } from "./Components/Radio";
-export { default as Select } from "./Components/Select";
+export { default as Select, Option } from "./Components/Select";
 export { default as Thumbnail } from "Components/Thumbnail";
 
 export * from "Components/Grid";
