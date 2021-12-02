d0463bafaac43ab9919df8e06be1d21060e3831c
Eduard Jacobs <edoter92@gmail.com>
2021-12-02 05:49:03 -0800

    test

--- rollup.config.ts
+++ rollup.config.ts
@@ -34,7 +34,7 @@ export default [
       }),
 
       external({
-        extensions: [".js", ".json", ".jsx", ".ts"],
+        packageJsonPath: "my/folder/package.json",
       }),
 
       resolve({
