47f98bef1fba37dee5746825f39cd48823ff00bb
Eduard Jacobs <edoter92@gmail.com>
2021-12-02 08:34:11 -0800

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,40 @@
-// export * from "./@types/global";
-// export { default as Layout } from "./Layout";
+export * from "./@types/global";
+export { default as Layout } from "./Layout";
 
-// export * from "./Components/Grid";
-// export * from "./Components/Input";
-// export * from "./Components/Divider";
-// export { default as Table, TableProps } from "./Components/Table";
-// export { default as Field, FieldProps } from "./Components/Field";
-// export { default as Badge, BadgeProps } from "./Components/Badge";
-// export { default as Radio, RadioProps } from "./Components/Radio";
-// export { default as Button, ButtonProps } from "./Components/Button";
-// export { default as Section, SectionProps } from "./Components/Section";
-// export { default as Checkbox, CheckboxProps } from "./Components/Checkbox";
-// export { default as Select, Option, SelectProps } from "./Components/Select";
-// export { default as Thumbnail, ThumbnailProps } from "./Components/Thumbnail";
-// export { Input, InputEmail, InputNumber, InputProps } from "./Components/Input";
-// export {
-//   default as Breadcrumb,
-//   BreadcrumbProps,
-// } from "./Components/Breadcrumb";
+export * from "./Components/Grid";
+export * from "./Components/Input";
+export * from "./Components/Divider";
+export { default as Table, TableProps } from "./Components/Table";
+export { default as Field, FieldProps } from "./Components/Field";
+export { default as Badge, BadgeProps } from "./Components/Badge";
+export { default as Radio, RadioProps } from "./Components/Radio";
+export { default as Button, ButtonProps } from "./Components/Button";
+export { default as Section, SectionProps } from "./Components/Section";
+export { default as Checkbox, CheckboxProps } from "./Components/Checkbox";
+export { default as Select, Option, SelectProps } from "./Components/Select";
+export { default as Thumbnail, ThumbnailProps } from "./Components/Thumbnail";
+export { Input, InputEmail, InputNumber, InputProps } from "./Components/Input";
+export {
+  default as Breadcrumb,
+  BreadcrumbProps,
+} from "./Components/Breadcrumb";
 
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
 
-import Layout from "./Layout";
-import Field from "Components/Field";
-import { Input } from "Components/Input";
+// import Layout from "./Layout";
+// import Field from "Components/Field";
+// import { Input } from "Components/Input";
 
-ReactDOM.render(
-  <Layout color="dark">
-    <Router>
-      <Field>
-        <Input placeholder="firstname" />
-        <Input placeholder="lastName" />
-      </Field>
-    </Router>
-  </Layout>,
-  document.getElementById("root")
-);
+// ReactDOM.render(
+//   <Layout color="dark">
+//     <Router>
+//       <Field>
+//         <Input placeholder="firstname" />
+//         <Input placeholder="lastName" />
+//       </Field>
+//     </Router>
+//   </Layout>,
+//   document.getElementById("root")
+// );
