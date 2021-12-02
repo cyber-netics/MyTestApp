0aaddf3ad5cf58bbec7b47a7f6ef4ce7b2eba1c5
Eduard Jacobs <edoter92@gmail.com>
2021-12-02 11:54:38 -0800

    test

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,17 @@ export { default as Layout } from "./Layout";
 export * from "./Components/Grid";
 export * from "./Components/Input";
 export * from "./Components/Divider";
-export { default as Table } from "./Components/Table";
 export { default as Field, FieldProps } from "./Components/Field";
 export { default as Badge, BadgeProps } from "./Components/Badge";
 export { default as Radio, RadioProps } from "./Components/Radio";
 export { default as Button, ButtonProps } from "./Components/Button";
 export { default as Section, SectionProps } from "./Components/Section";
+export { default as Table, TableRow, Content } from "./Components/Table";
 export { default as Checkbox, CheckboxProps } from "./Components/Checkbox";
 export { default as Select, Option, SelectProps } from "./Components/Select";
 export { default as Thumbnail, ThumbnailProps } from "./Components/Thumbnail";
 export { Input, InputEmail, InputNumber, InputProps } from "./Components/Input";
+
 export {
   default as Breadcrumb,
   BreadcrumbProps,
