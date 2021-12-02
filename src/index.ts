26db6745c653af24672c19db7ce6a1e0628662af
Eduard Jacobs <edoter92@gmail.com>
2021-12-01 22:40:19 -0800

    test

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
-export * from "./global";
+export * from "./types/global";
 
 export { default as Button, ButtonProps } from "./Components/Button";
 export { Input, InputEmail, InputNumber, InputProps } from "./Components/Input";
-export { default as Breadcrumb, BreadcrumbProps } from "Components/Breadcrumb";
+export {
+  default as Breadcrumb,
+  BreadcrumbProps,
+} from "./Components/Breadcrumb";
 
-export { default as Field } from "Components/Field";
+export { default as Field } from "./Components/Field";
 export { default as Badge } from "./Components/Badge";
 export { default as Checkbox } from "./Components/Checkbox";
 export { default as Radio } from "./Components/Radio";
 export { default as Select, Option } from "./Components/Select";
 export { default as Thumbnail } from "Components/Thumbnail";
 
-export * from "Components/Grid";
+export * from "./Components/Grid";
 export * from "./Components/Input";
