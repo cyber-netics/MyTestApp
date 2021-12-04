e0938982142bffc6fdd45cfbdf0cf5edf8d8bf11
Eduard Jacobs <edoter92@gmail.com>
2021-12-04 04:27:05 -0800

    test

--- src/Components/Payment/index.tsx
+++ src/Components/Payment/index.tsx
@@ -1,19 +1,31 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { inputStyles } from "Components/Input/styles";
+import { hightPrimary } from "Shared/styles";
 import {
   CardCvcElement,
   CardNumberElement,
   CardExpiryElement,
 } from "@stripe/react-stripe-js";
 
+const inputBase = css`
+  &&& {
+    ${hightPrimary};
+    background: #fff;
+    padding: 15px !important;
+  }
+`;
+
 export const CardCvc = styled(CardCvcElement)`
+  ${inputBase};
   ${inputStyles};
 `;
 
 export const CardExpiry = styled(CardExpiryElement)`
+  ${inputBase};
   ${inputStyles};
 `;
 
 export const CardNumber = styled(CardNumberElement)`
+  ${inputBase};
   ${inputStyles};
 `;
