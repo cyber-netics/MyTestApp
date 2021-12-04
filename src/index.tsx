e2eda395f471412c80b24fe86a3c61981aea3f0d
Eduard Jacobs <edoter92@gmail.com>
2021-12-04 01:19:08 -0800

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,45 +1,45 @@
-// export * from "./@types/global";
-// export { default as Layout } from "./Layout";
+export * from "./@types/global";
+export { default as Layout } from "./Layout";
 
-// export * from "./Components/Grid";
-// export * from "./Components/Input";
-// export * from "./Components/Divider";
-// export * from "./Components/ContentBox";
+export * from "./Components/Grid";
+export * from "./Components/Input";
+export * from "./Components/Divider";
+export * from "./Components/ContentBox";
 
-// export { default as Card, CardProps } from "./Components/Card";
-// export { default as Field, FieldProps } from "./Components/Field";
-// export { default as Badge, BadgeProps } from "./Components/Badge";
-// export { default as Radio, RadioProps } from "./Components/Radio";
-// export { default as Button, ButtonProps } from "./Components/Button";
-// export { default as Section, SectionProps } from "./Components/Section";
-// export { default as Table, TableRow, Content } from "./Components/Table";
-// export { default as Checkbox, CheckboxProps } from "./Components/Checkbox";
-// export { default as Select, Option, SelectProps } from "./Components/Select";
-// export { default as Thumbnail, ThumbnailProps } from "./Components/Thumbnail";
-// export { Input, InputEmail, InputNumber, InputProps } from "./Components/Input";
-// export {
-//   default as Breadcrumb,
-//   BreadcrumbProps,
-// } from "./Components/Breadcrumb";
+export { default as Card, CardProps } from "./Components/Card";
+export { default as Field, FieldProps } from "./Components/Field";
+export { default as Badge, BadgeProps } from "./Components/Badge";
+export { default as Radio, RadioProps } from "./Components/Radio";
+export { default as Button, ButtonProps } from "./Components/Button";
+export { default as Section, SectionProps } from "./Components/Section";
+export { default as Table, TableRow, Content } from "./Components/Table";
+export { default as Checkbox, CheckboxProps } from "./Components/Checkbox";
+export { default as Select, Option, SelectProps } from "./Components/Select";
+export { default as Thumbnail, ThumbnailProps } from "./Components/Thumbnail";
+export { Input, InputEmail, InputNumber, InputProps } from "./Components/Input";
+export {
+  default as Breadcrumb,
+  BreadcrumbProps,
+} from "./Components/Breadcrumb";
 
-import React from "react";
-import ReactDOM from "react-dom";
-import Layout from "Layout";
-import Section from "Components/Section";
-import { OptionBox } from "Components/ContentBox";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import Layout from "Layout";
+// import Section from "Components/Section";
+// import { OptionBox } from "Components/ContentBox";
 
-ReactDOM.render(
-  <Layout color="dark">
-    <div style={{ padding: "1rem" }}>
-      <Section title="Shipping method">
-        <OptionBox
-          items={[
-            { name: "Regular Shipping", result: "Free" },
-            { name: "Overnight Shipping", result: "$20" },
-          ]}
-        />
-      </Section>
-    </div>
-  </Layout>,
-  document.getElementById("root")
-);
+// ReactDOM.render(
+//   <Layout color="dark">
+//     <div style={{ padding: "1rem" }}>
+//       <Section title="Shipping method">
+//         <OptionBox
+//           items={[
+//             { name: "Regular Shipping", result: "Free" },
+//             { name: "Overnight Shipping", result: "$20" },
+//           ]}
+//         />
+//       </Section>
+//     </div>
+//   </Layout>,
+//   document.getElementById("root")
+// );
